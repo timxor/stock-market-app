f05a693f493faab76b7a8fb5df659234dfa3cd14
Aubrey Robinson <aubreyrobinson@aubreys-air.ds.usfca.edu>
2024-03-01 12:00:52 -0800

    making login Page

---
+++ stock-app/src/components/Firebase.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDgDpsAdofWkA9t-sw9QC_nrOQi7GuAneU",
+  authDomain: "stockapp-ebbaf.firebaseapp.com",
+  projectId: "stockapp-ebbaf",
+  storageBucket: "stockapp-ebbaf.appspot.com",
+  messagingSenderId: "1025307316182",
+  appId: "1:1025307316182:web:a4a49be5e759019cfd9e77",
+  measurementId: "G-CF8PDN9RC5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
--- stock-app/src/components/Login.js
+++ stock-app/src/components/Login.js
@@ -1,12 +1,30 @@
 // Login.js
 import React from 'react';
+import { useState } from 'react';
+import { auth } from 'firebase';
+import { signInWihEmailAndPassword } from 'firebase'
 
 const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const signIn = (e) =>{
+        e.preventDefault();//prevents reload so we dont lose email and password
+        signInWihEmailAndPassword(auth,email,password)
+        .then((userCredentials) => {
+            console.log(userCredentials);
+        }).catch((error) => {
+            console.log(error);
+        })
+    }
+
   return (
-    <div>
-      <h2>Login</h2>
-      <p>Explore our platform for the latest stock information and more.</p>
-      {/* Add other components or features */}
+    <div className='sign-in-container'>
+        <form onSubmit={signIn}>
+            <h2>Login</h2>
+            <input type='email' placeholder='Enter your email address' value={email} onChange={e => setEmail(e.target.value)} ></input>
+            <input type='password' placeholder='Enter your password'value={password} onChange={e => setPassword(e.target.value)}></input>
+            <button type="submit">Log In</button>
+        </form>
     </div>
   );
 };
