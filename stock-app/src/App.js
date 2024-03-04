e6001228c499e85100e30cf5334eb69bc8c153f0
Aubrey Robinson <aubreyrobinson@aubreys-air.ds.usfca.edu>
2024-03-04 02:10:38 -0800

    added basic signout feature may have it redirect to login page later

--- stock-app/src/App.js
+++ stock-app/src/App.js
@@ -5,10 +5,11 @@ import {
   Route,
 } from "react-router-dom";
 import Home from './components/Home';
-import Login from './components/SignIn.jsx';
+import Login from './components/auth/SignIn.jsx';
 import Stocks from './components/Stocks';
 import Profile from './components/Profile.js';
-import SignUp from './components/SignUp.jsx';
+import SignUp from './components/auth/SignUp.jsx';
+import AuthDetails from './components/AuthDetails.jsx';
 const App = () => {
   return (
     <Router>
@@ -16,8 +17,9 @@ const App = () => {
         <Route path="/Home" element={<Home />} />
         <Route path="/Login" element={<Login />} />
         <Route path="/Stocks" element={<Stocks />} />
-        <Route path="/Profile" element={<Profile />} />
+        <Route path="/Profile" element={<AuthDetails />} />
         <Route path="/SignUp" element={<SignUp />} />
+
         {/* Add other routes here */}
       </Routes>
     </Router>
