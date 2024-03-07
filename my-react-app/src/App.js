486b12617359d4966c993059fce5b5b775f7e82d
Aubrey Robinson <aubreyrobinson@Aubreys-MacBook-Air.local>
2024-03-07 02:14:25 -0800

    Change header name

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
     <>
       <Navbar bg="dark" variant="dark" className="header">
         <Container>
-          <Navbar.Brand href="#home">Library - Firebase CRUD</Navbar.Brand>
+          <Navbar.Brand href="#home">Stock App</Navbar.Brand>
         </Container>
       </Navbar>
 
