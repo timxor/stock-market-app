e0a936a9158f0023c8887a9cccd0ab392d1295a1
rrcoley09 <rrcoley@dons.usfca.edu>
2024-05-05 19:41:12 -0700

    adding home page message

--- my-react-app/src/components/HomePage.js
+++ my-react-app/src/components/HomePage.js
@@ -6,6 +6,12 @@ const HomePage = () => {
   return (
     <div className ="home-page-container">
       <h1>Welcome to TradeTrove!</h1>
+      <p>Whether you're a seasoned investor or just starting out, TradeTrove offers a suite of tools and resources to streamline 
+    your investment journey. From tracking your portfolio performance in real-time to accessing valuable market insights and analysis,
+    TradeTrove equips you with the knowledge and tools needed to navigate the complexities of the stock market with confidence. With 
+     TradeTrove, you can stay ahead of market trends, discover new investment opportunities, and take control of your financial future.
+      </p>
+    <h2>Get Started Today!</h2>
       <div>
         {/* Button to navigate to the login page */}
         <Link to="/login">
